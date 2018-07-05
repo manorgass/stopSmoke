478e6917d6244e55cd8235164a70e7e29d8e91fb
KorHacker <manorgass@gmail.com>
2018-07-06 05:55:22 +0900

    FAB 추가

--- app/src/main/java/com/korhacker/stopsmokemng/MainActivity.java
+++ app/src/main/java/com/korhacker/stopsmokemng/MainActivity.java
@@ -1,7 +1,14 @@
 package com.korhacker.stopsmokemng;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "add", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+            }
+        });
 
 
 
